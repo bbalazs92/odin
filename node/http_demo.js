d44556a50f5281439f2d66549747cb9ff44c493b
bbalazs92 <balazs.birta@gmail.com>
2023-04-10 12:54:56 +0200

    add os module to http_demo

--- node/http_demo.js
+++ node/http_demo.js
@@ -1,8 +1,23 @@
 const http = require('http');
+const os = require('os');
+
+// Create server object
+// http.createServer((req, res) => {
+//     // Write response
+//     res.write('Hello world');
+//     res.end();
+// }).listen(6001, () => console.log('Server running')); // don't use restricted ports like 6000
 
 // Create server object
 http.createServer((req, res) => {
     // Write response
-    res.write('Hello world');
+    res.write(os.platform());
     res.end();
-}).listen(6001, () => console.log('Server running')); // don't use restricted ports like 6000
+}).listen(6001, gatherInfo()); // don't use restricted ports like 6000
+
+function gatherInfo() {
+    console.log(os.platform());
+    console.log(os.arch());
+    console.log(os.release());
+    console.log(os.userInfo());
+};
